0477e63b5d46174322dc09081d9dd65e9f24edee
Grace <gracelidinskysmith@gmail.com>
2021-09-30 12:50:48 -0400

    Add left increase utility

--- Sources/SwiftCliCore/ChartConstructor.swift
+++ Sources/SwiftCliCore/ChartConstructor.swift
@@ -3,32 +3,41 @@ import Foundation
 public class ChartConstructor {
     public init() {}
     public func makeChart(stitchArray: [[String]]) -> String {
-
-        let numberOfRows = stitchArray.count
+        let lastRow = stitchArray.count - 1
+        var patternMetaData = OffsetUtility().gatherAllMetaData(stitchArray: stitchArray)
         var finishedChart = ""
-        let lastRow = numberOfRows - 1
-
-        for row in 0...lastRow {
-            let rowLength = stitchArray[row].count
-            if row == 0 {
-                finishedChart = makeBottomRow(width: rowLength) + finishedChart
-                finishedChart = makeStitchRow(row: stitchArray[row]) + finishedChart
-            } else {
-                let prevRowLength = stitchArray[row - 1].count
-                let rightSideDiff = (rowLength - prevRowLength)
-                if rightSideDiff != 0 {
-                    let middleLine = makeMiddleRowStitchCountChange(width: rowLength, rDiff: rightSideDiff, lDiff: 0)
-                    finishedChart =  middleLine + finishedChart
-                } else {
-                    finishedChart = makeMiddleRow(width: rowLength) + finishedChart
-                }
-
-                finishedChart = makeStitchRow(row: stitchArray[row]) + finishedChart
-            }
+        for row in 0...lastRow{
+            finishedChart = patternMetaData[row].totalRow + finishedChart
             if row == (lastRow) {
-                finishedChart = makeTopRow(width: rowLength) + finishedChart
+                finishedChart = makeTopRow(width: patternMetaData[row].width) + finishedChart
             }
         }
+//
+//        let numberOfRows = stitchArray.count
+//        var finishedChart = ""
+//        let lastRow = numberOfRows - 1
+//
+//        for row in 0...lastRow {
+//            let rowLength = stitchArray[row].count
+//            if row == 0 {
+//                finishedChart = makeBottomRow(width: rowLength) + finishedChart
+//                finishedChart = makeStitchRow(row: stitchArray[row]) + finishedChart
+//            } else {
+//                let prevRowLength = stitchArray[row - 1].count
+//                let rightSideDiff = (rowLength - prevRowLength)
+//                if rightSideDiff != 0 {
+//                    let middleLine = makeMiddleRowStitchCountChange(width: rowLength, rDiff: rightSideDiff, lDiff: 0)
+//                    finishedChart =  middleLine + finishedChart
+//                } else {
+//                    finishedChart = makeMiddleRow(width: rowLength) + finishedChart
+//                }
+//
+//                finishedChart = makeStitchRow(row: stitchArray[row]) + finishedChart
+//            }
+//            if row == (lastRow) {
+//                finishedChart = makeTopRow(width: rowLength) + finishedChart
+//            }
+//        }
         return(finishedChart)
     }
 
@@ -65,10 +74,10 @@ public class ChartConstructor {
         var rightStitches = ""
         var middleStitches = ""
         switch true {
+        case (lDiff > 0 && rDiff > 0):
+            middleStitches = String(repeating: "─┼", count: (width-rDiff-lDiff-1))
         case (rDiff > 0):
             middleStitches = String(repeating: "─┼", count: (width-rDiff-1))
-        case (rDiff < 0):
-            middleStitches = String(repeating: "─┼", count: (width-1))
         case (lDiff > 0):
             middleStitches = String(repeating: "─┼", count: (width-lDiff-1))
         default:
