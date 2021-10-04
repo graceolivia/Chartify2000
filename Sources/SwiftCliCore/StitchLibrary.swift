9ddc3168d642b91ba69c5fa80b8efffbee3d18c1
Grace <gracelidinskysmith@gmail.com>
2021-10-04 12:36:51 -0400

    Add stitchInfo struct for future ease of reference

--- Sources/SwiftCliCore/StitchLibrary.swift
+++ Sources/SwiftCliCore/StitchLibrary.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-let allowedStitches = ["k1", "p1", "ssk", "k2tog", "yo", "m1", "\n"]
+
 let stitchPrinting = ["k1": " ",
                       "p1": "-",
                       "ssk": "\\",
@@ -10,3 +10,18 @@ let stitchPrinting = ["k1": " ",
 
 let decreases = ["ssk", "k2tog"]
 let increases = ["m1", "yo"]
+
+struct stitchInfo {
+    var name: String
+    var incDecValue: Int = 0
+    var symbol: String
+}
+
+let allowedStitchesInfo = [stitchInfo(name: "k1", incDecValue: 0, symbol: " "),
+                           stitchInfo(name: "p1", incDecValue: 0, symbol: "-"),
+                           stitchInfo(name: "ssk", incDecValue: -1, symbol: "\\"),
+                           stitchInfo(name: "k2tog", incDecValue: -1, symbol: "/"),
+                           stitchInfo(name: "yo", incDecValue: 1, symbol: "o"),
+                           stitchInfo(name: "m1", incDecValue: 1, symbol: "m")]
+
+let allowedStitches = ["k1", "p1", "ssk", "k2tog", "yo", "m1", "\n"]
