a4b88190d9dfafc41123ed32943b2eaae09f5d57
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2021-10-14 10:17:23 -0400

    Add mock

--- Tests/SwiftCliCoreTests/ChartifyTest.swift
+++ Tests/SwiftCliCoreTests/ChartifyTest.swift
@@ -6,7 +6,13 @@ import Nimble
 class ChartifyFinishedTest: XCTestCase {
 
     func testRunValidatesTheInput() throws {
-    
+        let input = "p1 k1 k1 k1"
+        let inputValidator = InputValidatorMock()
+        
+        Chartify().run(input: input, inputValidator: inputValidator)
+        
+        expect(inputValidator.validateWasCalled).to(equal(true))
+        expect(inputValidator.validateArgument).to(equal(input))
     }
     
     func testRunTransformsTheInputIntoAnArrayIfInputIsValid() throws {
@@ -24,4 +30,25 @@ class ChartifyFinishedTest: XCTestCase {
     func testRunPrintsAnErrorMessageIfInputIsInvalid() throws {
     
     }
+    
+    // Put this in its own file
+    // A mock inherits from "superclass" you are testing
+    
+    class InputValidatorMock: InputValidator {
+        
+        public var validateWasCalled: Bool
+        public var validateArgument: String
+        
+        override public init() {
+            validateWasCalled = false
+            validateArgument = ""
+        }
+        
+        override public func validate(pattern: String) -> Bool {
+            validateWasCalled = true
+            validateArgument = pattern
+            return true
+        }
+    }
+    
 }
