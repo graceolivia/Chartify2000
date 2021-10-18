d90c30455b850cdf46be06d2c91fba409b1759af
Grace <gracelidinskysmith@gmail.com>
2021-10-18 17:16:35 -0400

    Read in file option

--- Sources/SwiftCli/main.swift
+++ Sources/SwiftCli/main.swift
@@ -6,11 +6,34 @@ struct StartProgram: ParsableCommand {
     @Argument(help: "Input the pattern.")
     var pattern: [String] = []
 
+    @Option(help: "File path")
+    var file: String?
+
+
     func run() {
         let inputValidator = InputValidator()
         let chartConstructor = ChartConstructor()
         let chartify = Chartify(inputValidator: inputValidator, chartConstructor: chartConstructor)
-        chartify.run(userInput: pattern)
+
+        if (file != nil) {
+            let filename = file
+
+            let contents = try! String(contentsOfFile: filename!)
+
+            let subLines = contents.split(separator:"\n")
+            let lines = subLines.map { String($0) }
+            print(lines)
+
+            chartify.run(userInput: lines)
+
+        }
+        else {
+            print(pattern)
+            chartify.run(userInput: pattern)
+        }
+
+
+
     }
 }
 
