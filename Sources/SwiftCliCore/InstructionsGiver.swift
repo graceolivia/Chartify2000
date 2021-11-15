95252fb8adfdc9eaf0ac3fa88ed73061bbec2d85
Grace <gracelidinskysmith@gmail.com>
2021-11-15 09:38:01 -0500

    Fix error message that include brackets

--- Sources/SwiftCliCore/InstructionsGiver.swift
+++ Sources/SwiftCliCore/InstructionsGiver.swift
@@ -9,11 +9,17 @@ public class InstructionsGiver {
         self.fileValidator = fileValidator
     }
     public func giveInstructions() throws {
-        let allowedFileTypes = fileValidator.allowedFileTypes
+
+
         var message = """
-Chartify takes in written patterns, either on the command line or in \(allowedFileTypes) files, and returns charts.
-Current allowed stitches are listed below.\n
+Chartify takes in written patterns, either on the command line or in allowed file types, and returns charts.
+
+Allowed file types include:\n
 """
+        for filetype in fileValidator.allowedFileTypes { message.append(filetype + "\n") }
+        message.append("""
+\nCurrent allowed stitches are listed below.\n\n
+""")
         message.append("Repeating stitches (can be written with any number 1 or greater afterwards: \n")
         for stitch in repeatingStitches {
             message.append(stitch.name + "\n")
